bb27c12bbb8407fc89a20c6d51661bd66d9dfc37
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2023-03-21 00:34:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-    createMerchant()
-    createMenu()
+    createApp();
 })
 
 async function getData() {
@@ -9,21 +8,131 @@ async function getData() {
     return data;
 }
 
-async function createMerchant() {
-    const data = await getData();
+async function createHeader(data) {
     const { merchant } = data;
-    const {
-        name,
-        logo,
-        telephone,
-        website,
-        address,
-        googleMap,
-        hours,
-        payment
-    } = merchant;
+    const { name, logo, contact, aboutRestaurant } = merchant;
+    const { telephone, email, website } = contact;
+    const { paragraph1, paragraph2 } = aboutRestaurant;
 
-    const j = hours.map(i => {
+    const app = document.getElementById('app');
+    const header = document.createElement('header');
+
+    header.innerHTML = `
+    <img src="images/${logo}" alt="${name}">
+    <h1>${telephone}</h1>
+    <div class="flex">
+        <a href="#">${website}</a>
+        <span>|</span>
+        <a href="#">${email}</a>
+    </div>
+    <div class="wrapper">
+        <p>${paragraph1}</p>
+        <p>${paragraph2}</p>
+    </div>
+    `;
+
+    app.appendChild(header);
+}
+
+async function createMenu(data) {
+    const { menu } = data;
+    const { categories } = menu;
+
+    categories.map(i => {
+        const { title, description, items } = i;
+        const app = document.getElementById('app');
+        const section = document.createElement('section');
+        const h2 = document.createElement('h2');
+        h2.innerText = `${title}`;
+        section.appendChild(h2);
+
+        if (description != null) {
+            const p = document.createElement('p');
+            p.innerText = `${description}`;
+            section.appendChild(p);
+        }
+
+        const ul = document.createElement('ul');
+        section.appendChild(ul);
+
+        items.map(i => {
+            const { id, item, description, price, subItems } = i;
+            const li = document.createElement('li');
+            ul.appendChild(li);
+
+            const h3 = document.createElement('h3');
+            h3.innerText = id ? `${id} • ${item}`: `${item}`;
+            li.appendChild(h3);
+
+            if (description != null) {
+                const p = document.createElement('p');
+                p.innerText = `${description}`;
+                li.appendChild(p);
+            }
+
+            if (price) {
+                const h4 = document.createElement('h4');
+                h4.innerText = `${price}`;
+                li.appendChild(h4);
+            }
+
+            if (subItems) {
+                const ul = document.createElement('ul');
+                li.appendChild(ul);
+
+                subItems.map(i => {
+                    const { id, subItem, description, price } = i;
+                    const li = document.createElement('li');
+                    ul.appendChild(li);
+
+                    const h3 = document.createElement('h3');
+                    h3.innerText = id ? `${id} • ${subItem}` : `${subItem}`;
+                    li.appendChild(h3);
+
+                    if (description != null) {
+                        const p = document.createElement('p');
+                        p.innerText = `${description}`;
+                        li.appendChild(p);
+                    }
+
+                    if (price) {
+                        const h4 = document.createElement('h4');
+                        h4.innerText = `${price}`;
+                        li.appendChild(h4);
+                    }
+                }).join('')
+            }
+        }).join('');
+        app.appendChild(section);
+    })
+}
+
+async function createReviews(data) {
+    const { merchant } = data;
+    const { customerReviews } = merchant;
+
+    const app = document.getElementById('app');
+    const section = document.createElement('section');
+
+    section.innerHTML = customerReviews.map(i => {
+        const { review, source } = i;
+        return `
+        <div class="wrapper">
+            <p>${review}</p>
+            <h5>- ${source}</h5>
+        </div>
+        `
+    }).join('');
+
+    app.appendChild(section);
+}
+
+async function createAbout(data) {
+    const { merchant } = data;
+    const { location, businessHours, payment } = merchant;
+    const { address, city, state, zipCode, googleMaps } = location;
+
+    const k = businessHours.map(i => {
         const { day, hours } = i;
         return `
         <tr>
@@ -33,149 +142,36 @@ async function createMerchant() {
         `
     }).join('');
 
-    const k = payment.map(i => {
-        const { name, image } = i;
+    const m = payment.map(i => {
+        const { item, image } = i;
         return `
         <figure>
-            <img src="images/${image}" alt="${name}" loading="lazy"/>
+            <img src="images/${image}" alt="${item}" loading="lazy"/>
         </figure>
         `
-    }).join('');
-
-    const img = document.createElement('img');
-    img.setAttribute('src', `images/${logo}`);
-    img.setAttribute('alt', `${name}`);
-    img.setAttribute('loading', 'lazy');
-    img.classList.add('logo');
-
-    const h2 = document.createElement('h2');
-    h2.innerText = telephone;
-
-    const anchor = document.createElement('a');
-    anchor.setAttribute('href', '#');
-    anchor.innerText = website;
-
-    const table = document.createElement('table');
-    const tbody = document.createElement('tbody');
-    table.appendChild(tbody);
-    tbody.innerHTML = `${j}`;
-
-    const h3 = document.createElement('h3');
-    h3.innerText = address;
-
-    const iframe = document.createElement('iframe');
-    iframe.setAttribute('src', `${googleMap}`);
-    iframe.setAttribute('loading', 'lazy');
-
-    const div = document.createElement('div');
-    div.classList.add('grid');
-    div.innerHTML = `${k}`;
-
-    const section = document.createElement('section');
-    section.appendChild(img);
-    section.appendChild(h2);
-    section.appendChild(anchor);
-    section.appendChild(table);
-    section.appendChild(h3);
-    section.appendChild(iframe);
-    section.appendChild(div);
+    }).join('')
 
     const app = document.getElementById('app');
+    const section = document.createElement('section');
+
+    section.innerHTML = `
+    <div class="wrapper">
+        <h2>${address} ${city}, ${state} ${zipCode}</h2>
+        <iframe src="${googleMaps}" loading="lazy" referrerpolicy="no-referrer-when-downgrade" allowfullscreen title="Google Maps"></iframe>
+    </div>
+    <table>
+        <tbody>${k}</tbody>
+    </table>
+    <div class="grid">${m}</div>
+    `;
+
     app.appendChild(section);
 }
 
-async function createMenu() {
+async function createApp() {
     const data = await getData();
-    const { menu } = data;
-    const { categories } = menu;
-
-    categories.map(category => {
-        const {
-            title,
-            description,
-            items
-        } = category;
-
-        const section = document.createElement('section');
-
-        const h2 = document.createElement('h2');
-        h2.innerText = title;
-        section.appendChild(h2);
-
-        if (description != null) {
-            const p = document.createElement('p');
-            p.innerText = description;
-            section.appendChild(p);
-        }
-
-        const ul = document.createElement('ul');
-        section.appendChild(ul);
-
-        items.map(i => {
-            const {
-                id,
-                item,
-                description,
-                price,
-                subItems
-            } = i;
-
-            const li = document.createElement('li');
-            ul.appendChild(li);
-
-            const h3 = document.createElement('h3');
-            h3.innerText = id ? id + ' • ' + item : item;
-            li.appendChild(h3);
-
-            if (description != null) {
-                const p = document.createElement('p');
-                p.innerText = description;
-                li.appendChild(p);
-            }
-
-            if (price) {
-                const h5 = document.createElement('h5');
-                h5.innerText = price;
-                li.appendChild(h5);
-            }
-
-            if (subItems) {
-                const subUl = document.createElement('ul');
-                li.appendChild(subUl);
-
-                subItems.map(i => {
-                    const {
-                        id,
-                        subItem,
-                        description,
-                        price
-                    } = i;
-
-                    const subLi = document.createElement('li');
-                    subUl.appendChild(subLi);
-
-                    const h4 = document.createElement('h4');
-                    h4.innerText = id ? id + ' • ' + subItem : subItem
-                    subLi.appendChild(h4);
-
-                    if (description != null) {
-                        const p = document.createElement('p');
-                        p.innerText = description;
-                        subLi.appendChild(p);
-                    }
-
-                    if (price) {
-                        const h5 = document.createElement('h5');
-                        h5.innerText = price;
-                        subLi.appendChild(h5);
-                    }
-
-                }).join('')
-            }
-        })
-
-        const app = document.getElementById('app');
-        app.appendChild(section);
-
-    }).join('');
+    createHeader(data);
+    createMenu(data);
+    createReviews(data);
+    createAbout(data);
 }
